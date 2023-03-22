94159d1403a4491e57134da2ed58cb876006480b
SHEF <Shefqetmush@gmail.com>
2023-03-21 20:05:06 -0400

    accept friend requests

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 import Navbar from '../components/Navbar';
 import UsersAside from '../components/UsersAside';
@@ -8,13 +9,26 @@ function Homepage({loggedInUser,setloggedInUser, port, allUsers}) {
   if (!loggedInUser) return <Login port={port} setloggedInUser={setloggedInUser}/>;
   
   const showFriendRequests = (friend) => {
+    const addFriend = async (loggedInUserId, userId) => {
+      try {
+        const response = await axios.post(`${port}/users/acceptFriendRequest`, {
+          userId,
+          loggedInUserId,
+        });
+        return response.data;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    };
+
     if (friend.receiver === "false" && friend.status === "pending"){
       const user = allUsers.find(user => user._id === friend.userId)
 
       return (
       <div>
         <p>{user.username} sent you a friend request</p>
-        <button>Add</button>
+        <button onClick={() => addFriend(loggedInUser.id, user._id)}>Add</button>
       </div>
       )
     }
